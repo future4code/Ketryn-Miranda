6b3b8578c5486cd7f4af8c0a71443535ba50dce2
Ketryn Silva <78522164+ketrynsdm@users.noreply.github.com>
2021-07-15 20:09:40 -0300

    Update index.js

--- quinzena1/variaveis/template_javascript/index.js
+++ quinzena1/variaveis/template_javascript/index.js
@@ -18,11 +18,23 @@ const valor3 = prompt("De onde é")
 
 console.log("Olá", valor1, "você tem", valor2, "anos. Onde", valor3, "e", valor2, "são os valores que o usuário inseriu.");
 //----------------------------------------------------------------------
-const olhos = true
-const camiseta = false
-const cadeira = true
+const pergunta1 = "Esta de olhos aberto?"
+const resposta1 = prompt(pergunta1)
+console.log(resposta1)
 
-console.log("Esta de olhos aberto?", olhos, "Usando camiseta?", camiseta, "Sentado na cadeira?", cadeira);
+console.log(pergunta1, "-", resposta1)
+
+const pergunta2 = "Usando camiseta??"
+const resposta2 = prompt(pergunta2)
+console.log(resposta2)
+
+console.log(pergunta2, "-", resposta2)
+
+const pergunta3 = "Sentado na cadeira??"
+const resposta3 = prompt(pergunta3)
+console.log(resposta3)
+
+console.log(pergunta3, "-", resposta3)
 //----------------------------------------------------------------------
 let a = 10
 let b = 25
